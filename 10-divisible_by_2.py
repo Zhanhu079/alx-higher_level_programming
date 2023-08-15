8009011775c660a5716ffb35fd67fdb450226392
vincent Victor <dev.victorvncent@gmail.com>
2023-08-14 21:42:50 -0700

    hot fix

--- 10-divisible_by_2.py
+++ 10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-idef divisible_by_2(my_list=[]):
+def divisible_by_2(my_list=[]):
     """Find all multiples of 2 in a list."""
     multiples = []
     for i in range(len(my_list)):
