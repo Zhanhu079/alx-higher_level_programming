f2df3a0cf289bb75f4aa48b4a05561679ced4ee0
vincent Victor <dev.victorvncent@gmail.com>
2023-09-12 19:47:31 -0700

    quick fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
-"""Reads file give"""
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as f:
-        file = (f.read())
-        print(file, end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
