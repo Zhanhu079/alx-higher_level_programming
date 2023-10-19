95a976c6cbc6a64f1130750c23d650ad1ed2f908
Vincent Victor <dev.victorvincent@gmail.com>
2023-10-19 11:36:30 -0700

    git fix groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,7 @@
 -- Lists the number of records with the same score in the table second_table in my MySQL server.
 -- Records are ordered by descending count.
+USE hbtn_0c_0;
+
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
