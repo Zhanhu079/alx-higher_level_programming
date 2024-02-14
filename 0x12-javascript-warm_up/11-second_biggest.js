8bdb21c05fd923b5cd01be836673f7b8c71a0c03
Victor Vincent <dev.victorvincent@gmail.com>
2024-02-14 14:34:10 -0800

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log(0);
+} else if (process.argv.length > 3) {
+  const input = process.argv.slice(2);
+  let largestNumber = Number.MIN_SAFE_INTEGER;
+  let secondLargest = Number.MIN_SAVE_INTEGER;
+
+  for (let i = 0; i < input.length; i++) {
+    const num = parseInt(input[i]);
+
+    if (num > largestNumber) {
+      secondLargest = largestNumber;
+      largestNumber = input[i];
+    }
+  }
+  console.log(secondLargest);
+} else {
+  console.log(1);
+}
