29e70d358b75941a9c7e631fd251d8a4ebcb5633
Victor Vincent <dev.victorvincent@gmail.com>
2024-02-14 15:22:10 -0800

    fix

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-function add(a,b)
-{
-	return parseInt(a) + parseInt(b);
-
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
-module.exports = {add};
+module.exports = { add };
